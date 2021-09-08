103d9cc95924ded058c0abcf9ddf2f9439f001d2
h0nda <41300123+h0nde@users.noreply.github.com>
2021-09-08 03:50:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ with open("requirements.txt") as fp:
 setuptools.setup(
     name="schttp",
     author="h0nda",
-    description="A faster alternate for the requests library",
     version=str(time.time()),
     url="https://github.com/h0nde/schttp",
     packages=setuptools.find_packages(),
